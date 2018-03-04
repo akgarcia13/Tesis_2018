60d75d40a71c2c862ca1a9d8f45fc7f96ef580a5
Astrid <akgarcia.13@est.ucab.edu.ve>
2018-03-04 03:09:31 -0400

    Heroku 11
    Intentando subir a Heroku 11

--- src/main/java/com/ucab/tesis/servicio/Configuracion.java
+++ src/main/java/com/ucab/tesis/servicio/Configuracion.java
@@ -5,6 +5,10 @@
  */
 package com.ucab.tesis.servicio;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import javax.ws.rs.ApplicationPath;
 
 import javax.ws.rs.core.Application;
@@ -14,10 +18,22 @@ import javax.ws.rs.core.Application;
  * @author Miguelangel
  */
 @ApplicationPath("api")
-public class Configuracion extends Application{
+public class Configuracion extends org.glassfish.jersey.server.ResourceConfig{
     
     public Configuracion()
     {
-    
+        super();
+        registerJsonProvider();
+        packages("com.ucab.tesis");
     }
+    /** register a provider to handle JSON request and response */
+	private void registerJsonProvider() {
+
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		mapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
+		JacksonJaxbJsonProvider provider = new JacksonJaxbJsonProvider();
+		provider.setMapper(mapper);
+		register(provider);
+}
 }

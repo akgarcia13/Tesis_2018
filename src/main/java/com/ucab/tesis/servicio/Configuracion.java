f0cc716bd0fb8b4bc5c144dfe427d47dc95ffded
Astrid <akgarcia.13@est.ucab.edu.ve>
2018-03-04 23:31:50 -0400

    Heroku 25
    Intento 25

--- src/main/java/com/ucab/tesis/servicio/Configuracion.java
+++ src/main/java/com/ucab/tesis/servicio/Configuracion.java
@@ -16,7 +16,7 @@ import javax.ws.rs.core.Application;
  *
  * @author Miguelangel
  */
-@ApplicationPath("/api/*")
+@ApplicationPath("api")
 public class Configuracion extends  Application {
     
   
